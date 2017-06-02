cb1d806eec7a108afe44d205673b2f4aded7c337
Nicholas Wilde <nwilde@conservice.com>
2017-06-02 09:55:07 -0600

    add responsive styling and clean up jquery for animations

--- pyslice/static/pyslice/js/main.js
+++ pyslice/static/pyslice/js/main.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
-    $(".nav-item a").mouseover(function(e){
-        $(this).children(".fa").addClass("spin");
-        $(this).children(".fa").removeClass("reverse-spin");
-        $(this).children(".menu-text").removeClass("hidden");
-        $(this).children(".menu-text").addClass("visible");
+    $(".nav-item a").mouseenter(function(e){
+        $(this).children(".fa").toggleClass("spin reverse-spin");
+        $(this).children(".menu-text").toggleClass("hidden visible");
     });
     $(".nav-item a").mouseleave(function(e){
-        $(this).children(".fa").removeClass("spin");
-        $(this).children(".fa").addClass("reverse-spin");
-        $(this).children(".menu-text").addClass("hidden");
-        $(this).children(".menu-text").removeClass("visible");
+        $(this).children(".fa").toggleClass("spin reverse-spin");
+        $(this).children(".menu-text").toggleClass("hidden visible");
     });
 });
