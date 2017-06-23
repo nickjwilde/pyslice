b60d7551be546ee02a6e523da19b6dbd7556cdbe
Nick Wilde <Nick Wilde>
2017-06-22 21:30:17 -0600

    add links to learn page for nvigation

--- website/urls.py
+++ website/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('pyslice.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^pyslice_admin/', admin.site.urls),
 ]
